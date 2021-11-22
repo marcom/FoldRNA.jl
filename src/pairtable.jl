dc77ee4ecfaaa930c043c95206bf28c667faccf1
Marco Matthies <marco.matthies@gmail.com>
2021-11-23 00:47:01 +0100

    Add Pairtable constructor from dot-bracket notation String

--- src/pairtable.jl
+++ src/pairtable.jl
@@ -1,5 +1,11 @@
 export Pairtable, StrandInfo
 
+const UNPAIRED = 0
+const UNPAIRED_CHAR = '.'
+const BRACKET_OPEN  = "([{<"
+const BRACKET_CLOSE = ")]}>"
+const NICK_CHAR = '+'
+
 Base.@kwdef struct StrandInfo
     startidx   :: Int
     endidx     :: Int
@@ -10,8 +16,6 @@ struct Pairtable
     pairs   :: Vector{Int}
     strands :: Vector{StrandInfo}
 end
-
-const UNPAIRED = 0
 
 function Pairtable(strandlengths::Integer...)
     # TODO: strands default to non-circular
@@ -37,3 +41,86 @@ end
 
 Base.length(pt::Pairtable) = length(pt.pairs)
 Base.:(==)(a::Pairtable, b::Pairtable) = (a.pairs == b.pairs) && (a.strands == b.strands)
+
+"""
+    Pairtable(dbn::String)
+
+Compute the pair table (base pairs and strand extents) from `dbn`, a
+nucleic acid secondary structure given in dot-bracket notation.
+
+In dot-bracket notation, unpaired bases are denoted by '.', base pairs
+are formed by pairing brackets '()[]{}<>', and strand ends (in a
+multi-strand complex) are indicated by '+'.
+
+Throws an exception if the dot-bracket string is malformed (illegal
+characters or wrong number of opening or closing parentheses).
+
+# Examples
+```jldoctest
+julia> Pairtable("(..)")
+Pairtable([4, 0, 0, 1], StrandInfo[StrandInfo(1, 4, false)])
+
+julia> Pairtable("(.(.[+.).].)")
+Pairtable([11, 0, 7, 0, 9, 0, 3, 0, 5, 0, 1], StrandInfo[StrandInfo(1, 5, false), StrandInfo(6, 11, false)])
+```
+"""
+function Pairtable(dbn::String)
+    # TODO: doesn't support circular strands
+    nbracket = length(BRACKET_OPEN)
+    nstrand = count(c -> c == NICK_CHAR, dbn) + 1
+    n = length(dbn) - (nstrand - 1)
+    stack = Vector{Vector{Int}}(undef, nbracket)
+    for k = 1:nbracket
+        stack[k] = Int[]
+    end
+    pairs = fill(UNPAIRED, n)
+    strands = Vector{StrandInfo}(undef, nstrand)
+    for k = 1:nstrand
+        strands[k] = StrandInfo(0, 0, false)
+    end
+    strands[1] = StrandInfo(1, 0, false)
+
+    i = 1
+    i_strand = 1
+    for c in dbn
+        if c == NICK_CHAR
+            strands[i_strand] = StrandInfo(strands[i_strand].startidx, i-1, false)
+            if strands[i_strand].startidx > strands[i_strand].endidx
+                error("complex has a strand with length 0 ending at position $(i + i_strand - 1)")
+            end
+            i_strand += 1
+            strands[i_strand] = StrandInfo(i, 0, false)
+            continue
+        end
+        idx_open = something(findfirst(isequal(c), BRACKET_OPEN), 0)
+        idx_close = something(findfirst(isequal(c), BRACKET_CLOSE), 0)
+        if c == UNPAIRED_CHAR
+            pairs[i] = UNPAIRED
+        elseif idx_open > 0
+            # opening bracket
+            push!(stack[idx_open], i)
+        elseif idx_close > 0
+            # closing bracket
+            if length(stack[idx_close]) > 0
+                j = pop!(stack[idx_close])
+                pairs[i] = j
+                pairs[j] = i
+            else
+                error("too many closing brackets of type '$(BRACKET_CLOSE[idx_close])' at position $(i + i_strand - 1)")
+            end
+        else
+            error("illegal character '$c' in dot-bracket string")
+        end
+        i += 1
+    end
+    for k = 1:nbracket
+        if length(stack[k]) != 0
+            error("more opening brackets than closing brackets of type '$(BRACKET_OPEN[k])' ($(length(stack[k])) more)")
+        end
+    end
+    strands[nstrand] = StrandInfo(strands[nstrand].startidx, n, false)
+    if strands[nstrand].startidx > strands[nstrand].endidx
+        error("complex has a strand with length 0 ending at position $n")
+    end
+    return Pairtable(pairs, strands)
+end
--- test/pairtable.jl
+++ test/pairtable.jl
@@ -17,9 +17,57 @@ using NucleicAcidFold
         pt = Pairtable(1, 2, 5)
         @test pt == Pairtable(
             zeros(Int, 8),
-            [ StrandInfo(1, 1, false), StrandInfo(2, 3, false), StrandInfo(4, 8, false) ]
+            [StrandInfo(1, 1, false),
+             StrandInfo(2, 3, false),
+             StrandInfo(4, 8, false)]
         )
         @test length(pt) == 8
     end
 
+    @testset "Parse non-pseudoknotted single" begin
+        @test Pairtable(".")     == Pairtable([0], [StrandInfo(1, 1, false)])
+        @test Pairtable("(.)")   == Pairtable([3, 0, 1],
+                                              [StrandInfo(1, 3, false)])
+        @test Pairtable("(.())") == Pairtable([5, 0, 4, 3, 1],
+                                              [StrandInfo(1, 5, false)])
+    end
+
+    @testset "Parse non-pseudoknotted complex" begin
+        @test Pairtable("(+)") ==
+            Pairtable([2, 1],
+                      [StrandInfo(1, 1, false), StrandInfo(2, 2, false)])
+        @test Pairtable("(().)+()") ==
+            Pairtable([5, 3, 2, 0, 1, 7, 6],
+                      [StrandInfo(1, 5, false), StrandInfo(6, 7, false)])
+    end
+
+    @testset "Parse pseudoknotted single" begin
+        @test Pairtable("([)]") ==
+            Pairtable([3, 4, 1, 2], [StrandInfo(1, 4, false)])
+    end
+
+    @testset "Parse pseudoknotted complex" begin
+        @test Pairtable("(+[+)]") ==
+            Pairtable([3, 4, 1, 2],
+                      [StrandInfo(1, 1, false),
+                       StrandInfo(2, 2, false),
+                       StrandInfo(3, 4, false)])
+    end
+
+    @testset "Parse illegal structures" begin
+        @test_throws ErrorException Pairtable("")
+        @test_throws ErrorException Pairtable(")")
+        @test_throws ErrorException Pairtable("(")
+        @test_throws ErrorException Pairtable("())")
+        @test_throws ErrorException Pairtable("()(")
+        @test_throws ErrorException Pairtable(")()")
+        @test_throws ErrorException Pairtable("(()")
+        @test_throws ErrorException Pairtable("[)")
+        @test_throws ErrorException Pairtable("(])")
+        @test_throws ErrorException Pairtable("()a")
+        @test_throws ErrorException Pairtable("+")
+        @test_throws ErrorException Pairtable("+(")
+        @test_throws ErrorException Pairtable("+.")
+        @test_throws ErrorException Pairtable(".+")
+    end
 end
