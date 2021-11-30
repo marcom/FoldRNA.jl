6d66d32a60cfb194e91aefa4675eae5c8af7b4bf
Marco Matthies <marco.matthies@gmail.com>
2021-11-30 13:51:35 +0100

    More tests for Alphabet

--- test/alphabet.jl
+++ test/alphabet.jl
@@ -3,6 +3,8 @@ using NucleicAcidFold: Alphabet, encode, decode
 
 @testset "Alphabet" begin
     al = Alphabet("RNA", "ACGU")
+    @test length(al) == 4
     @test encode(al, "GGCCUUAA") == [3, 3, 2, 2, 4, 4, 1, 1]
     @test decode(al, [3, 3, 2, 2, 4, 4, 1, 1]) == "GGCCUUAA"
+    @test Alphabet("ABC") == Alphabet("", "ABC")
 end
