67db86e9a97916e0601950c56cb9a09d729aa22c
Marco Matthies <marco.matthies@gmail.com>
2021-12-07 14:29:05 +0100

    Rearrange tests for Fold{BpModel}

--- test/fold.jl
+++ test/fold.jl
@@ -1,18 +1,20 @@
 using Test
-using NucleicAcidFold: Fold, RNA_BPMODEL, Pairtable, energy, mfe, partfn, prob_of_struct
+using NucleicAcidFold: Fold, RNA_BPMODEL, Pairtable, energy, mfe, partfn,
+    prob_of_struct
 using Unitful: Quantity
 
 @testset "Fold" begin
-    seq = "GGGAAACCC"
-    dbn = "(((...)))"
-    pt = Pairtable(dbn)
-    fold = Fold(seq, RNA_BPMODEL)
-    @test length(fold) == length(seq)
-    @test energy(fold, dbn) isa Quantity
-    @test energy(fold, pt) isa Quantity
-    en_mfe = mfe(fold)
-    @test en_mfe == -9.0u"kcal/mol"
-    @test partfn(fold) isa Quantity
-    @test prob_of_struct(fold, dbn) isa Float64
-    @test prob_of_struct(fold, pt) isa Float64
+    @testset "for BpModel" begin
+        seq = "GGGAAACCC"
+        dbn = "(((...)))"
+        pt = Pairtable(dbn)
+        fold = Fold(seq, RNA_BPMODEL)
+        @test length(fold) == length(seq)
+        @test energy(fold, dbn) isa Quantity
+        @test energy(fold, pt) isa Quantity
+        @test mfe(fold) == -9.0u"kcal/mol"
+        @test partfn(fold) isa Quantity
+        @test prob_of_struct(fold, dbn) isa Float64
+        @test prob_of_struct(fold, pt) isa Float64
+    end
 end
