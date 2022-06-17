5fb265dc107c80c764c14ba40928cbba1826511f
Marco Matthies <marco.matthies@gmail.com>
2022-06-17 13:07:35 +0200

    fix type in partfn

--- test/loopmodel.jl
+++ test/loopmodel.jl
@@ -1,10 +1,21 @@
 using Test
 using NucleicAcidFold: bptype
+using Unitful: Quantity
 
 @testset "loopmodel" begin
     @testset "LoopModel" begin
         model = LoopModel{Float64,Int,4,6,30}(alphabet=Alphabet("ACGU"))
         @test model isa LoopModel
         @test bptype(model, 1, 1) isa Int
+        seq = "GGGAAACCC"
+        fc = Fold(seq, model)
+        @test mfe(fc) isa Quantity
+        @test partfn(fc) isa Quantity
+
+        model = RNA_TURNER2004
+        seq = "GGGAAACCC"
+        fc = Fold(seq, model)
+        @test mfe(fc) isa Quantity
+        @test partfn(fc) isa Quantity
     end
 end
