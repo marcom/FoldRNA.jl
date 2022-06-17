10fb9b7b04d1c5032814a6e53cc578d555ec8eb2
Marco Matthies <marco.matthies@gmail.com>
2022-06-17 13:04:27 +0200

    fix semiring type for partfn

--- src/fold.jl
+++ src/fold.jl
@@ -92,7 +92,8 @@ function mfe(fold::Fold{M}) where {T, M <: LoopModel{T}}
 end
 
 function partfn(fold::Fold{M}) where {T, M <: LoopModel{T}}
-    A, Ab, Am, Am1 = loopmodel(LogSR{T}, fold; hpmin=fold.model.hpmin, maxintloop=-1)
+    Tsr = Float64
+    A, Ab, Am, Am1 = loopmodel(LogSR{Tsr}, fold; hpmin=fold.model.hpmin, maxintloop=-1)
     logQ = A[1, length(fold)].val
     return - fold.model.RT * logQ
 end
