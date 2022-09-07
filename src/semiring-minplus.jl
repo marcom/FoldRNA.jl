c2f7696f0764aa6ae6e096bbd023e6887ce0e6a5
Marco Matthies <marco.matthies@gmail.com>
2022-09-07 17:25:16 +0200

    mfe-plus semiring zero cannott be typemax for integer types

--- src/semiring-minplus.jl
+++ src/semiring-minplus.jl
@@ -10,6 +10,8 @@ Base.:(==)(x::MinPlusSR{T}, y::MinPlusSR{T}) where {T} = x.val == y.val
 Base.:+(x::MinPlusSR{T}, y::MinPlusSR{T}) where {T} = MinPlusSR{T}(min(x.val, y.val))
 Base.:*(x::MinPlusSR{T}, y::MinPlusSR{T}) where {T} = MinPlusSR{T}(x.val + y.val)
 Base.zero(::Type{MinPlusSR{T}}) where {T} = MinPlusSR{T}(typemax(T))
+# TODO: for integers, we can't use typemax(T) as additions (mul in semiring) would overflow
+Base.zero(::Type{MinPlusSR{T}}) where {T<:Integer} = MinPlusSR{T}(T(round(typemax(T)/8)))
 Base.one(::Type{MinPlusSR{T}}) where {T} = MinPlusSR{T}(zero(T))
 Base.:+(x::MinPlusSR{T}, y::Number) where {T} = x + MinPlusSR{T}(T(y))
 Base.:+(x::Number, y::MinPlusSR{T}) where {T} = MinPlusSR{T}(T(x)) + y
